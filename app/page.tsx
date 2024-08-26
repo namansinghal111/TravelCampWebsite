149dc08050651ec69c5a84e5cc86aa23cfc950db
namansinghal <72150803+namansinghal111@users.noreply.github.com>
2024-08-26 20:32:39 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -7,11 +7,11 @@ import Hero from "@/components/Hero";
 export default function Home() {
   return (
     <>
-     <Hero id="Hero" />
-      <Camp id="camp" />
-      <Guide id="guide" />
-      <Features id="features" />
-      <GetApp id="getapp" />
+     <Hero />
+      <Camp />
+      <Guide />
+      <Features />
+      <GetApp />
     </>
   );
 }
